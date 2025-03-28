0174adbae3ebb05448efc3f907df1f1b5949468f
Trinh Nguyen <nta.toan@gmail.com>
2025-03-28 13:24:01 -0400

    update doc

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # prismjs-loader
-This script provides a fast and efficient way to integrate Prism.js syntax highlighting into web pages.
+
+If you want to quickly highlight the code syntax on your web pages without scream throught PrismJS documentation page for the integration. Here is the link just for you
+
+```html
+	<script src="https://cdn.jsdelivr.net/gh/vespaiach/prismjs-loader@v1.0.0/dist/prismjs-loader.js"
+    integrity="sha384-KyZXEJ+M9uNQ0V8vJrL+J6uyNKjk5N2C4F6joDdDd8Hl5y2KlgkZe6z8Of7RoG0T"
+    crossorigin="anonymous" defer></script>
+```
--- build.js
+++ build.js
@@ -1,10 +1,9 @@
 const esbuild = require('esbuild')
-const { version } = require('./package.json')
 
 esbuild
   .build({
     entryPoints: ['prismjs-loader.js'],
-    outfile: `dist/prismjs-loader-${version}.js`,
+    outfile: `dist/prismjs-loader.js`,
     bundle: true,
     platform: 'browser',
     minify: true,
@@ -12,6 +11,6 @@ esbuild
     target: ['es2020', 'chrome58', 'edge58', 'firefox58', 'safari13']
   })
   .then(() => {
-    console.log(`Build completed: output-${version}.js`)
+    console.log(`Build completed: prismjs-loader.js`)
   })
   .catch(() => process.exit(1))
